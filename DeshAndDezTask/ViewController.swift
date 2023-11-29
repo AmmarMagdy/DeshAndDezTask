1aaa9d875e31ad64e5cfe7dcadc57fd68155fef5
Ammar Magdy <ammar.m.badawi@gmail.com>
2023-11-29 10:24:33 +0200

    first commit

---
+++ DeshAndDezTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeshAndDezTask
+//
+//  Created by Ammar.M on 29/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
