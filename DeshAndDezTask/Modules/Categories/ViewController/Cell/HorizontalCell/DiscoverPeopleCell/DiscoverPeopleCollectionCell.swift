ec3bb8e6dd970bce46cf2a8cbb678fbec1566284
Ammar Magdy <ammar.m.badawi@gmail.com>
2023-11-29 17:29:06 +0200

    edit follow button

--- DeshAndDezTask/Modules/Categories/ViewController/Cell/HorizontalCell/DiscoverPeopleCell/DiscoverPeopleCollectionCell.swift
+++ DeshAndDezTask/Modules/Categories/ViewController/Cell/HorizontalCell/DiscoverPeopleCell/DiscoverPeopleCollectionCell.swift
@@ -23,5 +23,22 @@ class DiscoverPeopleCollectionCell: UICollectionViewCell {
         imageView.image = data.image
         specialistLabel.text = data.specialist
         followButton.setTitle(data.isFollowedByMe ? "UnFollow" : "Follow", for: .normal)
+        if data.isFollowedByMe {
+            unfollow()
+        } else {
+            follow()
+        }
+    }
+    
+    func follow() {
+        followButton.setTitle("Follow", for: .normal)
+        followButton.backgroundColor = #colorLiteral(red: 1, green: 0.7220000029, blue: 0.00400000019, alpha: 1)
+    }
+    
+    func unfollow() {
+        followButton.setTitle("UnFollow", for: .normal)
+        followButton.backgroundColor = #colorLiteral(red: 0.1099999994, green: 0.1220000014, blue: 0.1840000004, alpha: 1)
+        followButton.layer.borderColor = #colorLiteral(red: 1, green: 0.7220000029, blue: 0.00400000019, alpha: 1)
+        followButton.layer.borderWidth = 1
     }
 }
